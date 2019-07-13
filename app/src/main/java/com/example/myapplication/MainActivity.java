50afd6a56e3840749c0f493ee6754dee08e560e2
Sameer Malik <sameermalik20aug@gmail.com>
2019-07-14 00:11:55 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.Btn1);
+        btn2 = findViewById(R.id.Btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent n_intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(n_intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent e_intent = new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(e_intent);
+            }
+        });
+    }
+}
